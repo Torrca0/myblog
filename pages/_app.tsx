50bbc127c33e1ed3bb49b1035df536639e9124cb
Torrca <116360839+Torrca0@users.noreply.github.com>
2023-03-08 14:53:42 +0900

    骨組み

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import Layout from '../components/Layout';
+import '../styles/globals.css';
+
+type MyAppProps = {
+  Component: any,
+  pageProps: JSX.Element
 }
+
+function MyApp({ Component, pageProps }: MyAppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
